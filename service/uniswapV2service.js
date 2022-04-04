aa6c340c4be77e61f7383607edb6fc342a0bdd13
Abil Manoj <abilmanoj@Abils-MacBook-Pro.local>
2022-04-04 13:59:35 +0530

    issue with uniswap v2 fixed

--- service/uniswapV2service.js
+++ service/uniswapV2service.js
@@ -5,7 +5,6 @@ const web3 = new Web3(
 const fs = require("fs");
 
 const UniswapV2FactoryABI = require("../contractABI/FactoryContractABI/uniswapV2Factory.json");
-const res = require("express/lib/response");
 
 const UniswapV2Factory = new web3.eth.Contract(
   UniswapV2FactoryABI,
@@ -22,10 +21,6 @@ loadFiles.split(/\r?\n/).forEach((line) => {
   lineArray.push(line);
 });
 
-lineArray.forEach((line, index) => {
-  addressExtractor(line, index);
-});
-
 function addressExtractor(line, index) {
   const tempArray = line.split(":");
   const tempArray2 = tempArray[1].split(",");
@@ -33,32 +28,24 @@ function addressExtractor(line, index) {
   return address[1];
 }
 
-for (i = 0; i < lineArray.length - 1; i++) {
-  for (j = i + 1; j < lineArray.length; j++) {
-    const address1 = web3.utils.toChecksumAddress(
-      addressExtractor(lineArray[i])
-    );
-    const address2 = web3.utils.toChecksumAddress(
-      addressExtractor(lineArray[j])
-    );
+async function wrapperFunction() {
+  for (i = 0; i < lineArray.length - 1; i++) {
+    for (j = i + 1; j < lineArray.length; j++) {
+      const address1 = web3.utils.toChecksumAddress(
+        addressExtractor(lineArray[i])
+      );
+      const address2 = web3.utils.toChecksumAddress(
+        addressExtractor(lineArray[j])
+      );
 
-    // console.log("addresses: ", lineArray[i], "  ", lineArray[j]);
-    callChainFunction(address1, address2).then((response) => {
+      const response = await UniswapV2Factory.methods
+        .getPair(address1, address2)
+        .call();
       if (!(response == "0x0000000000000000000000000000000000000000")) {
         console.log("response : ", response);
       }
-    });
+    }
   }
 }
 
-async function callChainFunction(address1, address2) {
-  const response = await UniswapV2Factory.methods
-    .getPair(address1, address2)
-    .call();
-  return response;
-}
-
-// callChainFunction(
-//   "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-//   "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
-// );
+wrapperFunction();
